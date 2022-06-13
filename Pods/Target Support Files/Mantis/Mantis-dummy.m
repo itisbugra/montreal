5886d66be26fc9c995deec0759c58b75261f4357
Buğra Ekuklu <ekuklu@icloud.com>
2022-06-13 23:55:38 +0100

    git: remove files from sc

--- Pods/Target Support Files/Mantis/Mantis-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Mantis : NSObject
-@end
-@implementation PodsDummy_Mantis
-@end
--- Pods/Target Support Files/Pods-QuesslyTests/Pods-QuesslyTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_QuesslyTests : NSObject
-@end
-@implementation PodsDummy_Pods_QuesslyTests
-@end
