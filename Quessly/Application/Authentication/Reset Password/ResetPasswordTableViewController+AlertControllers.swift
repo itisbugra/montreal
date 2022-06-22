534322b9e9cfc1aa3caf1ed12eb817fece881db6
respawm1 <respawm19@gmail.com>
2022-06-22 21:35:32 +0100

    i18n: tr strings for sign up

--- Quessly/Application/Authentication/Reset Password/ResetPasswordTableViewController+AlertControllers.swift
+++ Quessly/Application/Authentication/Reset Password/ResetPasswordTableViewController+AlertControllers.swift
@@ -3,19 +3,22 @@ import UIKit
 extension ResetPasswordTableViewController {
   func presentResetPasswordCodeAlert(completion: ((String?) -> Void)?) {
     DispatchQueue.main.async {
-      let alertController = UIAlertController(title: "Verification Code",
-                                              message: "Enter the verification code you have received with the e-mail.",
+      let alertController = UIAlertController(title: NSLocalizedString("Verification Code", comment: ""),
+                                              message: NSLocalizedString("Enter the verification code you have received with the e-mail.", comment: ""),
                                               preferredStyle: .alert)
       
       let codeTextField = alertController.addTextField {
         $0.keyboardType = .numberPad
-        $0.font = UIFont(name: "Menlo", size: 11.0)
+        $0.font = .monospacedSystemFont(ofSize: 11.0, weight: .regular)
         $0.textAlignment = .center
+        $0.clearButtonMode = .whileEditing
       }
       
-      [UIAlertAction(title: "Continue", style: .default) { _ in
+      [UIAlertAction(title: NSLocalizedString("Continue", comment: ""),
+                     style: .default) { _ in
         print(alertController.textFields!.first!.text!)
-      }, UIAlertAction(title: "Cancel", style: .cancel) { _ in
+      }, UIAlertAction(title: NSLocalizedString("Cancel", comment: ""),
+                       style: .cancel) { _ in
         self.dismiss(animated: true, completion: nil)
       }].forEach { alertController.addAction($0) }
       
@@ -33,13 +36,13 @@ extension ResetPasswordTableViewController {
   
   func presentSendingIndicatorAlert(completion: (() -> Void)?) {
     DispatchQueue.main.async {
-      let alertController = UIAlertController(title: "Please wait...",
-                                              message: nil,
+      let alertController = UIAlertController(title: NSLocalizedString("Please wait...", comment: ""),
+                                              message: NSLocalizedString("Sending a verification code to your e-mail address.", comment: ""),
                                               preferredStyle: .alert)
       
       alertController.addActivityIndicator()
       
-      [UIAlertAction(title: "Cancel", style: .cancel)].forEach { alertController.addAction($0) }
+      [UIAlertAction(title: NSLocalizedString("Cancel", comment: ""), style: .cancel)].forEach { alertController.addAction($0) }
       
       self.present(alertController, animated: true, completion: completion)
     }
